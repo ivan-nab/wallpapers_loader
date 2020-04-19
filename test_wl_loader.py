1648b147e5b9683436da143905c3394e6d17b68b
unknown <ivan.nab@yandex.ru>
2020-04-19 16:46:13 +0300

    тесты и рефаторинг PEP-8

--- test_wl_loader.py
+++ test_wl_loader.py
@@ -1,7 +1,8 @@
 import os
-from wl_loader import construct_url, parse_web_page, download_wallpaper
 from datetime import datetime
 from unittest import mock
+
+from wl_loader import construct_url, download_wallpaper, parse_web_page, download_wallpapers
 
 
 class TestConstructUrl:
@@ -13,40 +14,51 @@ class TestConstructUrl:
         date = datetime.strptime("11-2019", "%m-%Y")
         url = construct_url(date)
         assert url == self.expected_url
-    
+
     def test_construct_url_last_month(self):
         date = datetime.strptime('12-2019', "%m-%Y")
         url = construct_url(date)
         assert url == self.expected_url_last_month
 
+
 class TestParseWebPage:
     def setup(self):
-        with open ("test_html.html") as f:
+        with open("test_html.html") as f:
             self.page_body = f.read()
         self.expected_images_count = 21
         self.expected_image_url = 'http://files.smashingmagazine.com/wallpapers/jan-17/reindeer/nocal/jan-17-reindeer-nocal-640x480.png'
-    
+
     def test_parse_web_page_with_data(self):
         parsed_data = parse_web_page(self.page_body)
         assert len(parsed_data['640x480']) == self.expected_images_count
         assert self.expected_image_url in parsed_data['640x480']
-    
+
     def test_parse_web_page_without_data(self):
         parsed_data = parse_web_page('<html></html>')
         assert parsed_data == {}
-        
+        parsed_data = parse_web_page(None)
+        assert parsed_data == {}
+
 
 class TestDownloadWallpaper:
     def setup(self):
         self.wallpaper_file = ".\\test_data\\feb-14-principles-of-good-design--dieter-rams-nocal-1920x1080.png"
         self.expected_size = os.stat(self.wallpaper_file).st_size
+        self.expected_parsed_data = {
+            '640x480': [
+                'http://example.com/1.jpg', 'http://example.com/2.jpg',
+                'http://example.com/3.jpg'
+            ]
+        }
 
     @mock.patch('wl_loader.requests.get')
     def test_download_wallpaper(self, mock_requests_get):
         wallpaper_fd = open(self.wallpaper_file, "rb")
         mock_requests_get.return_value = wallpaper_fd
         mock_requests_get.return_value.status_code = 200
-        filename = download_wallpaper("http://localhost/feb-14-principles-of-good-design--dieter-rams-nocal-1920x1080.png")
+        filename = download_wallpaper(
+            "http://localhost/feb-14-principles-of-good-design--dieter-rams-nocal-1920x1080.png"
+        )
         assert os.path.exists(filename)
         assert os.stat(filename).st_size == self.expected_size
         os.remove(filename)
@@ -57,6 +69,30 @@ class TestDownloadWallpaper:
         mock_requests_get.return_value = mock.Mock()
         mock_requests_get.return_value.status_code = 404
         filename = download_wallpaper("http://wrongurl/picture.jpg")
-        assert filename == None
+        assert filename is None
+
+    @mock.patch('wl_loader.download_wallpaper')
+    @mock.patch('wl_loader.parse_web_page')
+    @mock.patch('wl_loader.get_data')
+    def test_download_wallpapers(self, mock_get_data, mock_parse_web_page,
+                                 mock_download_wallpaper):
+        mock_get_data.return_value = ''
+        mock_parse_web_page.return_value = self.expected_parsed_data
+        input_date = datetime.strptime('12-2020', '%m-%Y')
+        input_resolution = "640x480"
+        download_wallpapers(input_date, input_resolution)
+        assert mock_get_data.call_count == 1
+        assert mock_download_wallpaper.call_count == 3
 
 
+    @mock.patch('wl_loader.download_wallpaper')
+    @mock.patch('wl_loader.parse_web_page')
+    @mock.patch('wl_loader.get_data')
+    def test_download_wallpapers_incorrect_params(self, mock_get_data, mock_parse_web_page,
+                                 mock_download_wallpaper):
+        mock_get_data.return_value = ''
+        mock_parse_web_page.return_value = self.expected_parsed_data
+        input_date = datetime.strptime('12-2020', '%m-%Y')
+        input_resolution = "1920x1080"
+        download_wallpapers(input_date, input_resolution)
+        assert mock_download_wallpaper.call_count == 0
--- wl_loader.py
+++ wl_loader.py
@@ -1,40 +1,45 @@
+import calendar
 import os
-import requests
+import time
 from datetime import datetime
 from urllib.parse import urljoin
+import requests
 from lxml import etree
-import calendar
+
 import settings
 
 
 def parse_web_page(page_body):
-    tree = etree.HTML(page_body)
+    tree = etree.HTML(str(page_body))
     result = tree.xpath(settings.XPATH_REF)
     links_dict = {}
     for r in result:
-        links_dict.setdefault(r.text, []).append(r.attrib['href']) 
+        links_dict.setdefault(r.text, []).append(r.attrib['href'])
     return links_dict
+
 
 def get_data(url):
     response = requests.get(url)
     if response.status_code == 200:
         return response.content
 
+
 def construct_url(date):
     month = date.month
     year = date.year
-    
+
     month_str = str(month).zfill(2)
 
     if month == 12:
         collection_month = 1
-        collection_year = year + 1 
+        collection_year = year + 1
     else:
         collection_month = month + 1
         collection_year = year
     collection_month_name = calendar.month_name[collection_month].lower()
     walpapers_page_url = f"{year}/{month_str}/desktop-wallpaper-calendars-{collection_month_name}-{collection_year}/"
-    return urljoin(settings.WALLPAPERS_URL,walpapers_page_url)
+    return urljoin(settings.WALLPAPERS_URL, walpapers_page_url)
+
 
 def download_wallpaper(url):
     response = requests.get(url, stream=True)
@@ -46,40 +51,41 @@ def download_wallpaper(url):
             os.mkdir(wallpaper_dir)
         filepath = os.path.join(wallpaper_dir, wallpaper_filename)
         print(f'downloading {url} to {filepath}')
-        with open (filepath, 'wb') as f:
+        with open(filepath, 'wb') as f:
             for chunk in response:
                 f.write(chunk)
         return filepath
 
 
 def download_wallpapers(date, resolution):
-    url = construct_url(input_date)
+    url = construct_url(date)
     data = get_data(url)
     parsed_data = parse_web_page(data)
     if parsed_data:
-        links_list = parsed_data.get(input_resolution)
+        links_list = parsed_data.get(resolution)
         if links_list:
             for link in links_list:
                 download_wallpaper(link)
+        else:
+            print(f"Wallpapers with resolution {resolution} not found")
     else:
         print('Wallpappers not found')
 
 
 def get_date_from_string(input_date):
     try:
-        wl_date = datetime.strptime(input_date,"%m-%Y")
+        wl_date = datetime.strptime(input_date, "%m-%Y")
         return wl_date
     except ValueError:
         print("incorrect month and year parameter")
-    
+
 
 def validate_resolution(input_resolution):
     try:
-        res_x, res_y = map(int,input_resolution.split('x'))
+        res_x, res_y = map(int, input_resolution.split('x'))
         return input_resolution
     except ValueError:
         print("incorrect resolution parameter")
-    
 
 
 if __name__ == "__main__":
@@ -87,11 +93,14 @@ if __name__ == "__main__":
     if len(sys.argv) > 2:
         input_date = get_date_from_string(sys.argv[1])
         input_resolution = validate_resolution(sys.argv[2])
+        if input_date and input_resolution:
+            start_time = time.time()
+            download_wallpapers(input_date, input_resolution)
+            print("time :", time.time() - start_time)
     else:
-        print('Wallpapers loader\n \
-                usage: wl_loader.py <month-year> <resolution>\n \
-                example: wl_loader.py 12-2020 1920x1080\n\
-            ')
+        print('''
+            Wallpapers loader\n
+                usage: wl_loader.py <month-year> <resolution>\n
+                example: wl_loader.py 12-2020 1920x1080\n
+            ''')
 
-    if input_date and input_resolution:
-        download_wallpapers(input_date, input_resolution)
